06383e8ee7ed8c2d2f3a2c3736c79e6f5944676e
vhhbr01 <romboe@gmx.at>
2019-03-18 17:46:07 +0100

    +

---
+++ datasets/belgien.js
@@ -0,0 +1,13 @@
+sb.data = {
+people: [
+{id:0,uuid:'leo1',firstname:'Leopold I',lastname:'',birth:'1790-12-16',death:'1865-12-10',mother:undefined,father:undefined},
+{id:1,uuid:'louisedorleans',firstname:'Louise d’Orléans',lastname:'',birth:'1812-04-03',death:'1850-10-11',mother:undefined,father:undefined},
+{id:2,uuid:'leo2',firstname:'Leopold II',lastname:'',birth:'1835-04-09',death:'1909-12-17',mother:1,father:0},
+{id:3,uuid:'phil1835',firstname:'Philipp von Belgien',lastname:'',birth:'1837-03-24',death:'1905-11-17',mother:1,father:0},
+{id:4,uuid:'marialuise1837',firstname:'Maria Luise',lastname:'von Hohenzollern-Sigmaringen',birth:'1845-11-17',death:'1912-11-26',mother:undefined,father:undefined},
+{id:5,uuid:'albert1',firstname:'Albert I',lastname:'',birth:'1875-04-08',death:'1934-02-17',mother:4,father:3},
+],
+bild: {
+	'leo1': '1.jpg'
+}
+}
